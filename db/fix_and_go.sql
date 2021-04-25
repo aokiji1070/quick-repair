1a85d5676dd6e92e6f74c7102c35123f47f068a9
aokiji1070 <younesessalhi@hotmail.fr>
2021-04-25 21:49:42 +0200

    adding db template

---
+++ db/fix_and_go.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : Dim 25 avr. 2021 à 19:48
+-- Version du serveur :  8.0.21
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `fix_and_go`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `repair_list`
+--
+
+DROP TABLE IF EXISTS `repair_list`;
+CREATE TABLE IF NOT EXISTS `repair_list` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `client_name` varchar(255) NOT NULL,
+  `arriving` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `model` varchar(255) NOT NULL,
+  `trouble` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `remarks` text NOT NULL,
+  `total_price` decimal(10,0) NOT NULL,
+  `deposit` decimal(10,0) NOT NULL,
+  `barcode` int DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
