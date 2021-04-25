f3c441717f092af4fbdf08cb9a20bd9efdcbdc7d
aokiji1070 <younesessalhi@hotmail.fr>
2021-04-25 21:44:15 +0200

    binding login page with adming page

--- admin_page.php
+++ admin_page.php
@@ -1,3 +1,14 @@
+<?php
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
+if($_SESSION['loggedIn']){
+      //allow
+}
+else{
+      //redirect to the login page
+      header('Location: login_page.html'); 
+}
+?>
 <!DOCTYPE html>
 <html>
 
@@ -7,7 +18,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
-        <title>formulaire de réparation</title>
+        <title>Page Login</title>
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
